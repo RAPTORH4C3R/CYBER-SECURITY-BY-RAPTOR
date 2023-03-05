14525cef080a4ea254a26470a9b878def109f82b
RAPTORH4C3R <88148333+RAPTORH4C3R@users.noreply.github.com>
2023-03-06 00:31:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,63 +1,59 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-	<title>Login Page</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			background-color: #f1f1f1;
-		}
-		
-		.container {
-			padding: 20px;
-			background-color: #fff;
-			border-radius: 10px;
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-			margin: auto;
-			margin-top: 50px;
-			max-width: 400px;
-		}
-		
-		input[type=text], input[type=password] {
-			width: 100%;
-			padding: 12px 20px;
-			margin: 8px 0;
-			display: inline-block;
-			border: 1px solid #ccc;
-			box-sizing: border-box;
-			border-radius: 5px;
-		}
-		
-		button {
-			background-color: #4CAF50;
-			color: white;
-			padding: 14px 20px;
-			margin: 8px 0;
-			border: none;
-			border-radius: 5px;
-			cursor: pointer;
-			width: 100%;
-		}
-		
-		button:hover {
-			opacity: 0.8;
-		}
-	</style>
+
+<meta name="viewport" content="with=device-width, initial scale=1.0">
+
+<title>cyber security</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Road+Rage&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="style.css">
+
 </head>
+
 <body>
-	<div class="container">
-		<h1>Login</h1>
-		<form action="" method="post">
-			<label for="username"><b>Username</b></label>
-			<input type="text" placeholder="Enter Username" name="username" required>
 
-			<label for="password"><b>Password</b></label>
-			<input type="password" placeholder="Enter Password" name="password" required>
+<section class="header">
 
-			<button type="submit">Login</button>
-		</form>
-	</div>
+<nav>
+
+    <a href="index.html"><img src="raptor.png"></a>
+
+    <div class="nav-links">
+
+    <ul>
+
+         <li><a href="about.html">ABOUT RAPTOR</a></li>
+
+   
+
+         <li><a href="contact.html">CONTACT</a></li>
+
+    </ul>
+
+    </div>
+
+    </nav>
+
+   <div class="text-box">
+
+   <h1> What is Cyber Security?</h1>
+
+   <p>Cyber security is the practice of defending<br computers, servers, mobile devices, electronic systems, networks, and data from malicious attacks.<br> Its also known as information technology security or electronic information security.</p>
+
+                                    <a href="more.html" class="hero-btn">MORE INFO</a>
+
+   </div>
+
+</section>
+
 </body>
+
 </html>
